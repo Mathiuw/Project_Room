21cfac02b49fbc9ba2a7c42739db7e5932638048
Mathiuw <mateus.augusto2003@hotmail.com>
2022-03-17 16:04:09 -0300

    Volume Control Working

---
+++ Assets/Scripts/VolumeControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class VolumeControl : MonoBehaviour
+{
+    private Slider volumeSlider;
+
+    private void Awake()
+    {
+        volumeSlider = transform.Find("VolumeSlider").GetComponent<Slider>();
+    }
+
+    private void Start()
+    {
+        volumeSlider.value = AudioListener.volume;
+    }
+
+    private void Update()
+    {
+        AudioListener.volume = volumeSlider.value;
+    }
+}
