9b2d5ae96d39ce0b1f63ef208f96b88e4d921466
Mathiuw <mateus.augusto2003@hotmail.com>
2022-05-13 17:39:44 -0300

    Camera Rotate Sideways(WIP)

---
+++ Assets/Scripts/CameraRotateSideways.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotateSideways : MonoBehaviour
+{
+    [SerializeField] float angleLimit;
+    [SerializeField] float smooth;
+    [Header("Angle")]
+    [SerializeField]float angle;
+
+    GameObject playerCamera;
+    Rigidbody rb;
+
+    void Awake()
+    {
+        rb = transform.root.GetComponentInChildren<Rigidbody>();
+        playerCamera = GameObject.Find("Main Camera");
+    }
+
+    void Update()
+    {
+        Transform cameraTransform = playerCamera.transform;
+
+        cameraTransform.eulerAngles = new Vector3(cameraTransform.eulerAngles.x, cameraTransform.eulerAngles.y, RotateVector());
+    }
+
+    float RotateVector()
+    {
+        if (Input.GetKey(KeyCode.A)) angle += smooth;    
+        else if (Input.GetKey(KeyCode.D)) angle -= smooth;
+        else
+        {
+            if (angle > 0) angle -= smooth;
+            else if (angle < 0) angle += smooth;
+        }
+
+        if (angle > angleLimit) angle = angleLimit;
+        else if (angle < -angleLimit) angle = -angleLimit;
+
+        return angle;
+    }
+}
