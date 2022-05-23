a671836aa09fe1d040cef3489efbf25370abc2db
Mathiuw <mateus.augusto2003@hotmail.com>
2022-05-23 14:52:46 -0300

    Dynamic DOF (WIP)

---
+++ Assets/Scripts/DynamicDOF.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+using UnityEngine.Animations;
+
+public class DynamicDOF : MonoBehaviour
+{
+    [SerializeField] float focusSpeed;
+    float maxDistance = 5f;
+    float distance;
+    PostProcessVolume postProcessVolume;
+
+    void Awake()
+    {
+        postProcessVolume = GetComponent<PostProcessVolume>();
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        DepthOfField dof;
+        bool isAiming = transform.root.GetComponent<Animator>().GetBool("isAiming");
+        
+        postProcessVolume.profile.TryGetSettings<DepthOfField>(out dof);
+        if (Physics.Raycast(transform.position, transform.forward, out hit, maxDistance) && !isAiming)
+        {
+            distance = Vector3.Distance(transform.position, hit.point);
+        }
+        else distance = maxDistance;
+        dof.focusDistance.value = distance;//Mathf.Lerp(dof.focusDistance.value, distance, Time.deltaTime * focusSpeed);
+    }
+}
