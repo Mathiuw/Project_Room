a911bad9fdb9db17431b4be2f20aecdf8d40756d
Mateus Sartori <98856859+Mathiuw@users.noreply.github.com>
2023-01-31 17:15:48 -0300

    0.7.9.8
    -Elevator Code Rework
    (without animations yet)

---
+++ Assets/_Scripts/ElevatorAnimations.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Animator))]
+public class ElevatorAnimations : MonoBehaviour
+{
+    Animator animator;
+
+    void Awake() => animator = GetComponent<Animator>();
+
+    void Start() 
+    {
+        SetState(false);
+
+        ElevatorInteraction[] interactions = GetComponentsInChildren<ElevatorInteraction>();
+
+        foreach (ElevatorInteraction interaction in interactions)
+        {
+            interaction.onButtomPress += InvertState;
+        }
+    }
+
+    void SetState(bool b) => animator.SetBool("open", b);
+
+    void InvertState() 
+    {
+        bool state = animator.GetBool("open");
+
+        animator.SetBool("open", !state);
+    }
+}
